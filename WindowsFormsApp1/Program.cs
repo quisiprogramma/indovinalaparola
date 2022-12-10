25a6e5172339b5cfcf12b4053ebe89028bfcc5bb
quisiprogramma <>
2022-12-10 14:23:08 +0100

    Prima Commit

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        private const int MAX_ATTEMPTS = 6;
+        private const int STRING_LENGTH = 5;
+
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            bool gioca = true;
+
+            while (gioca)
+            {
+                PrintIntro();
+                gioca = Gioca();
+                Console.Clear();
+            }
+             
+        }
+
+        private static void PrintIntro()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string greeting = @"
+
+  _____           _            _              
+  \_   \_ __   __| | _____   _(_)_ __   __ _  
+   / /\/ '_ \ / _` |/ _ \ \ / / | '_ \ / _` | 
+/\/ /_ | | | | (_| | (_) \ V /| | | | | (_| | 
+\____/ |_| |_|\__,_|\___/ \_/ |_|_| |_|\__,_| 
+                                              
+                 __                           
+                / /  __ _                     
+               / /  / _` |                    
+              / /__| (_| |                    
+              \____/\__,_|                    
+                                              
+         ___                _                 
+        / _ \__ _ _ __ ___ | | __ _           
+       / /_)/ _` | '__/ _ \| |/ _` |          
+      / ___/ (_| | | | (_) | | (_| |          
+      \/    \__,_|_|  \___/|_|\__,_|          
+                                              
+
+               ";
+            Console.WriteLine(greeting);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static bool Gioca()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Ho pensato ad una parola di 5 Lettere, indovinala!");
+            Console.WriteLine("Hai a disposizione ben " + MAX_ATTEMPTS + " tentativi.");
+            WordUtils wu = new WordUtils("word_source.txt");
+            //Console.WriteLine("Cheat: " + wu.Word);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine(); Console.WriteLine();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            int attempts = 0; bool guess = false;
+            while (attempts < MAX_ATTEMPTS)
+            {
+
+                String inputWord = ""; bool validAttempt = false;
+                while (inputWord.Length != STRING_LENGTH || !validAttempt)
+                {
+                    Console.WriteLine();
+                    Console.Write("Tentativo #" + (attempts + 1) + ": ");
+                    inputWord = Console.ReadLine();
+                    if (inputWord.Length != STRING_LENGTH)
+                    {
+                        Console.WriteLine("La parola deve essere composta da " + STRING_LENGTH +" caratteri!");
+                        Console.WriteLine();
+                    }
+                    if (WordUtils.ValidAttempt(inputWord))
+                    {
+                        validAttempt = true;
+                    }
+                    else
+                    {
+                        validAttempt = false;
+                        Console.WriteLine("La parola deve contenere solo caratteri testuali!");
+                        Console.WriteLine();
+                    }
+                }
+                attempts++;
+                inputWord = inputWord.ToUpper();
+                List<Letter> userWord = wu.CheckWord(inputWord);
+                foreach (Letter letter in userWord)
+                {
+                    Console.BackgroundColor = letter.Color;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(letter.Character);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" ");
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                if (wu.Word == inputWord)
+                {
+                    Console.WriteLine("OTTIMO! Hai indovinato in " + (attempts) + ((attempts == 1) ? " tentativo." : " tentativi."));
+                    guess = true;
+                    break;
+                }
+                else
+                {
+                    wu.PrintAlphabet();
+                    Console.WriteLine("La Parola non è esatta! Hai ancora " + (MAX_ATTEMPTS - attempts) + ((attempts == MAX_ATTEMPTS - 1) ? " tentativo" : " tentativi") + " a disposizione");
+                }
+
+            }
+            if (!guess)
+            {
+                Console.WriteLine("La parola esatta era: " + wu.Word);
+                Console.WriteLine("Avevi 5 possibilità per indovinarla, sarà per la prossima volta!");
+            }
+            Console.WriteLine();
+            Console.Write("Vuoi giocare ancora (s/n)? ");
+            ConsoleKeyInfo response = new ConsoleKeyInfo();
+            while (response.KeyChar !='s' && response.KeyChar != 'S' && response.KeyChar != 'n' && response.KeyChar != 'N')
+                response = Console.ReadKey();
+            Console.WriteLine();
+            return (response.KeyChar == 's' || response.KeyChar == 'S');
+        }
+    }
+}
---
+++ WindowsFormsApp1/WordUtils.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public class WordUtils
+    {
+        public string Word { get; set; }
+        public Dictionary<Char, ConsoleColor> lettersList;
+        private const string ALPHABET = "ABCDEFGHILMNOPQRSTUVZ";
+
+        public WordUtils(string sourcePath)
+        {
+            initDictionary();
+            this.Word = FindWord(sourcePath).ToUpper();
+        }
+
+        private void initDictionary()
+        {
+            lettersList = new Dictionary<char, ConsoleColor>();
+            for (int i=0; i< ALPHABET.Length; i++)
+            {
+                this.lettersList.Add(ALPHABET[i], ConsoleColor.Gray);
+            }
+        }
+
+        public void PrintAlphabet()
+        {
+            for (int i = 0; i < ALPHABET.Length; i++)
+            {
+                Console.BackgroundColor = this.lettersList[ALPHABET[i]];
+                Console.Write(ALPHABET[i]);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+        }
+
+        public WordUtils(string word, bool force)
+        {
+            this.Word = word;
+        }
+
+        public string FindWord(string filePath)
+        {
+            var fileName = Environment.CurrentDirectory + Path.DirectorySeparatorChar +  filePath;
+            //Console.WriteLine(fileName);
+            var file = File.ReadLines(fileName).ToList();
+            int count = file.Count();
+            Random rnd = new Random();
+            int skip = rnd.Next(0, count);
+            return file.Skip(skip).First();
+        }
+
+        public List<Letter> WordToLetters(string toCheck)
+        {
+            List<Letter> result = new List<Letter>();
+            for (int i = 0; i < toCheck.Length; i++)
+            {
+                result.Add(item: new Letter(toCheck[i], ConsoleColor.Red));
+            }
+            return result;
+        }
+
+        public List<Letter> CheckWord(string paramWord)
+        {
+            if (paramWord.Length != Word.Length)
+                throw new Exception("Dimensione sbagliata");
+            List<Letter> result = WordToLetters(paramWord);
+            string tempWord = Word;
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] == paramWord[i])
+                {
+                    result[i].Color = ConsoleColor.Green;
+                    tempWord = replaceChar(tempWord, i, ' ');
+                    this.lettersList[paramWord[i]] = ConsoleColor.Green;
+                }
+            }
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (tempWord.Contains(paramWord[i]) && (result[i].Color != ConsoleColor.Green))
+                {
+                    result[i].Color = ConsoleColor.Yellow;
+                    tempWord = tempWord.Replace(paramWord[i], ' ');
+                    this.lettersList[paramWord[i]] = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    if (!Word.Contains(paramWord[i]))
+                        this.lettersList[paramWord[i]] = ConsoleColor.Red;
+                }
+            }
+            for (int i = 0; i < paramWord.Length; i++)
+            {
+                if (Word.Contains(paramWord[i]))
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+            }
+            return result;
+        }
+
+
+
+
+        private string replaceChar(string tempWord, int index, char newChar)
+        {
+            StringBuilder sb = new StringBuilder(tempWord);
+            sb[index] = newChar;
+            return sb.ToString();
+        }
+
+        public static bool ValidAttempt(string attempt)
+        {
+            return Regex.IsMatch(attempt, @"^[a-zA-Z]+$");
+        }
+
+
+    }
+
+    public class Letter
+    {
+
+        public Char Character { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Letter(Char c, ConsoleColor co)
+        {
+            this.Character = c;
+            this.Color = co;
+        }
+
+        public override string ToString()
+        {
+            return "[" + this.Character + " - " + this.Color + "]";
+         }
+    }
+}
